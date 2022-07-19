022c041cd71d6b9f62afa360ce82ffbe9e155711
Sneha <snhthakare@gmail.com>
2022-07-19 11:52:34 +0530

    Adds test cases for login form

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Login from './components/Login'
 
-test('renders learn react link', () => {
+test('renders title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/login form/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders username text', () => {
+  render(<Login />);
+  const inputText = screen.getByPlaceholderText('username')
+  expect(inputText).toBeInTheDocument();
+});
+
+test('renders password text', () => {
+  render(<Login />);
+  const inputText = screen.getByPlaceholderText('password')
+  expect(inputText).toBeInTheDocument();
+});
+
+test('renders button text', () => {
+  render(<Login />);
+  const inputNode = screen.getByRole('button')
+  expect(inputNode).toBeInTheDocument();
+});
+
+test('input text should be empty', () => {
+  render(<Login />);
+  const inputText = screen.getByPlaceholderText('username')
+  expect(inputText.value).toBe('');
+});
+
+test('password text should be empty', () => {
+  render(<Login />);
+  const passwordText = screen.getByPlaceholderText('password')
+  expect(passwordText.value).toBe('');
 });
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h3 id="heading">Login form</h3>
+            <div><input type="text" placeholder='username' /></div>
+            <div><input type="password" placeholder='password' /></div>
+            <div><button>Login</button></div>
+        </div>
+    )
+}
+
+export default Login
