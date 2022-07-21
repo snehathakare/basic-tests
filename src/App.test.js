df8b48bbca19902ee7b612d3c249b8531b6decc5
Sneha <snhthakare@gmail.com>
2022-07-21 10:37:33 +0530

    Updates test cases for login form

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 import Login from './components/Login'
 
@@ -37,3 +37,34 @@ test('password text should be empty', () => {
   const passwordText = screen.getByPlaceholderText('password')
   expect(passwordText.value).toBe('');
 });
+
+test('user input should change', () => {
+  render(<Login />);
+  const userText = screen.getByPlaceholderText('username')
+  const testValue = "test"
+  fireEvent.change(userText, { target: { value: testValue } })
+  expect(userText.value).toBe(testValue);
+});
+
+test('password input should change', () => {
+  render(<Login />);
+  const passwordText = screen.getByPlaceholderText('password')
+  const testValue = "test"
+  fireEvent.change(passwordText, { target: { value: testValue } })
+  expect(passwordText.value).toBe(testValue);
+});
+
+test('button should not be disabled when input exists', () => {
+  render(<Login />);
+  const loginButton = screen.getByRole('button')
+  const userText = screen.getByPlaceholderText(/username/i)
+  const passwordText = screen.getByPlaceholderText(/password/i)
+
+  const testValue = "test";
+
+  fireEvent.change(userText, { target: { value: testValue } })
+  fireEvent.change(passwordText, { target: { value: testValue } })
+  expect(loginButton).not.toBeDisabled();
+});
+
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [error, setError] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users/1")
+        setUser(data)
+    }
     return (
         <div>
             <h3 id="heading">Login form</h3>
-            <div><input type="text" placeholder='username' /></div>
-            <div><input type="password" placeholder='password' /></div>
-            <div><button>Login</button></div>
+            <div><input type="text" placeholder='username' onChange={(e) => setUsername(e.target.value)} /></div>
+            <div><input type="password" placeholder='password' onChange={(e) => setPassword(e.target.value)} /></div>
+            <span data-test-id="error"
+                style={{ visibility: error ? "visible" : "hidden" }}
+            >
+                Something went wrong!
+            </span>
+            <div><button disabled={!username || !password} onClick={handleLogin}>Login</button></div>
         </div>
     )
 }
